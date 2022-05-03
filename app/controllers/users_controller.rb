fbc76f85d7d0319b4d673ff9d625149796e7267d
masato sawada <vazaluta@gmail.com>
2022-05-03 05:44:33 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,40 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+  
+  def new
+    @user = User.new
+    # debugger
+  end
+  
+  # POST /users (+ params)
+  def create
+    # (user + given params).save
+    # User.create(params[:user])
+    # => name, email, pass/confirmation
+    @user = User.new(user_params)
+    if @user.save
+      # Success (valid params)
+      # GET "/users/#{@user.id}"  <= 結論
+      flash[:success] = "Welcome to the Sample App!"    # hashのように使えるメソッド、一時的に表示
+      redirect_to user_url(@user)
+      # redirect_to user_path(@user)
+      # => redirect_to user_path(@user.id)
+      # => redirect_to user_path(1)
+      # => /user/1  
+    else
+      render 'new'
+      #Failure
+    end
+  end
+  
+  private 
+  
+    def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'staticpages#about'
   get  '/contact', to: 'staticpages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users
 end
